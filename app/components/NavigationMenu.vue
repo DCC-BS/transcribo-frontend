8f86c31890818cd7e70d9d705298afc6cd6a830a
Yanick Schraner <yanick.schraner@bs.ch>
2025-07-29 11:27:21 +0200

    Fix navbar layout

--- app/components/NavigationMenu.vue
+++ app/components/NavigationMenu.vue
@@ -116,8 +116,12 @@ function handleRedo(): void {
 <template>
     <div>
         <ClientOnly>
-            <UNavigationMenu content-orientation="vertical" variant="link" :items="items"
-                class="w-full justify-between align-top z-50">
+            <UNavigationMenu 
+                content-orientation="horizontal" 
+                variant="link" 
+                :items="items"
+                class="w-full grid grid-cols-3 items-center z-50 [&>*:nth-child(1)]:justify-self-start [&>*:nth-child(2)]:justify-self-center [&>*:nth-child(3)]:justify-self-end"
+            >
                 <template #disclaimer>
                     <DisclaimerButton variant="ghost" />
                 </template>
