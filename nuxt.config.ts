5e4b58b3e00f63be66b7b175ef6b734fe35e49f6
Tobias Bollinger <tobias.bollinger@bs.ch>
2025-09-18 11:26:37 +0200

    Add CORS support and create ping API endpoint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,19 @@ import pwaIcons from "./public/icons.json";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     compatibilityDate: "2024-11-01",
+    routeRules: {
+        "/api/ping": {
+            cors: true,
+            headers: {
+                "Cache-Control": "no-store",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "GET",
+                "Access-Control-Allow-Headers":
+                    "Origin, Content-Type, Accept, Authorization, X-Requested-With",
+                "Access-Control-Allow-Credentials": "true",
+            },
+        },
+    },
     runtimeConfig: {
         githubToken: process.env.GITHUB_TOKEN,
         apiUrl: process.env.API_URL,
